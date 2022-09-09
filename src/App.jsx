93c9db6b3faa3af73a2f2eddd841b82d5f7601c3
OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
2022-09-09 16:54:50 -0500

    react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "./components/Footer";
 
 export default function App(){
     return(
         <>
             <h1>My React Solo Project!</h1>
             <h1>Test 2</h1>
+            <Footer />
         </>
     )
 }
