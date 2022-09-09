a67ad3e7656295be2027f75a851e8cdd684f9a97
OmarAlexDev <69879119+OmarAlexDev@users.noreply.github.com>
2022-09-09 16:18:01 -0500

    testing update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function App(){
     return(
-        <h1>My React Solo Project!</h1>
+        <>
+            <h1>My React Solo Project!</h1>
+            <h1>Test 2</h1>
+        </>
     )
 }
